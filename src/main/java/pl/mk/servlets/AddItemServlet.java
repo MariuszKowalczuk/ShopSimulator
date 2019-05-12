48ea963246db614155cb60016cc27e7e7181fc2b
Mariusz Kowalczuk <lordmer@gmail.com>
2019-05-12 14:07:58 +0200

    working cart

--- src/main/java/pl/mk/servlets/AddItemServlet.java
+++ src/main/java/pl/mk/servlets/AddItemServlet.java
@@ -25,14 +25,17 @@ public class AddItemServlet extends HttpServlet {
 
         Integer quantity = Integer.valueOf(req.getParameter("quantity"));
         Integer product_id = Integer.valueOf(req.getParameter("product_id"));
-        Cart cart = new Cart();
-        Product product = phr.findById(product_id);
-        cart.addItem(product, quantity);
         HttpSession session = req.getSession();
-        session.setAttribute("cart", cart);
+        Product product = phr.findById(product_id);
+        Cart cart = (Cart) session.getAttribute("cart");
+        if (cart == null) {
+            cart = new Cart();
+            session.setAttribute("cart", cart);
+        }
+
+        cart.addItem(product, quantity);
 
         //dodać ify, że jeśli nie ma, to utworzyć i że jeśli nie, to dcaodać
-
 
 
     }
